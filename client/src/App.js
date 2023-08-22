b8d0fe04d283c35dbfecbc679f94a114aba0b7b3
maqiie <99504010+maqiie@users.noreply.github.com>
2023-08-23 00:03:46 +0300

    started the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter, Route, and Routes
 import './App.css';
-import Register from './components/register';
+import Register from './components/register/register';
+import Home from './components/Home/Home'; // Update the path based on your actual structure
+
+
+
 function App() {
   return (
-    <div>
-      <Register/>
-
-    </div>
+    <Router>
+      <div className="App">
+        {/* Header, navigation, or other common components can go here */}
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path='/' element={<Home />} />
+      
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
