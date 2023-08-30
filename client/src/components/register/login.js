1f638c85f655eb2a927e0889eab8413ae533c4d7
maqiie <candiemp2580@gmail.com>
2023-08-31 00:05:01 +0300

    linked signup and login with backend

--- client/src/components/register/login.js
+++ client/src/components/register/login.js
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = ({ onToggleForm }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    // Handle login logic here
-    console.log('Logging in with:', email, password);
+
+    try {
+      const response = await axios.post('http://localhost:3001/auth/sign_in', {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        // Login successful, you can handle redirection or other actions here
+        console.log('Login successful');
+      } else {
+        // Handle login error
+        console.log('Login failed');
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
   };
 
   return (
