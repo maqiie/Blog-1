9e8d4c531604bf61f18622ecc589c3b3b1920940
maqiie <candiemp2580@gmail.com>
2023-09-03 04:31:46 +0300

    authorized and autenticated users

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,128 @@
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import BrowserRouter, Route, and Routes
+// import React, { useState, useEffect } from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import "./App.css";
-// import Home from "./components/Home/Home"; // Update the path based on your actual structure
+// import Home from "./components/Home/Home";
 // import Navbar from "./components/Navbar/Navbar";
 // import Footer from "./components/Footer/Footer";
 // import AuthComponent from "./components/register/AuthComponent";
 // import Sidebar from "./components/Sidebar/Sidebar";
 // import Blog from "./components/Blogs/Blogs";
 // import CreatePost from "./components/createpost/createpost";
+// import Health from "./components/Categories/Health";
+// import Travel from "./components/Categories/Travel";
+// import Music from "./components/Categories/Music";
+// import Art from "./components/Categories/Art";
+// import Education from "./components/Categories/Education";
+// import Fashion from "./components/Categories/Fashion";
+// import Other from "./components/Categories/Other";
+// import Science from "./components/Categories/Science";
+// import Sports from "./components/Categories/Sports";
+// import Technology from "./components/Categories/Technology";
+// import ProtectedRoute from "./components/ProtectedRoute"; // Import the ProtectedRoute component
+
 // function App() {
+//   // const [token, setToken] = useState(localStorage.getItem("token"));
+
+//   // const onLogin = (userToken) => {
+//   //   // Set the token in both state and localStorage
+//   //   setToken(userToken);
+//   //   localStorage.setItem("token", userToken);
+//   // };
+
+//   // const onLogout = () => {
+//   //   // Clear the token from both state and localStorage
+//   //   setToken(null);
+//   //   localStorage.removeItem("token");
+//   // };
+
+//   // useEffect(() => {
+//   //   // Check if the user is already authenticated (has a token in localStorage)
+//   //   const storedToken = localStorage.getItem("token");
+//   //   if (storedToken) {
+//   //     setToken(storedToken);
+//   //   }
+//   // }, []);
+//   // Inside your useEffect or wherever you need to check authentication
+//   // const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("authToken"));
+
+//   const storedToken = localStorage.getItem("authToken"); // Retrieve Bearer token from local storage
+
+//   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("authToken"));
+
+
 //   return (
 //     <Router>
 //       <div className="App">
-//         {/* Header, navigation, or other common components can go here */}
-        
-//         <Sidebar/>
-//         <Navbar/>
-        
+//         <Sidebar />
+//         <Navbar />
+
 //         <Routes>
 //           <Route path="/" element={<Home />} />
-//         <Route path="/auth" element={<AuthComponent/>} />
-//         <Route path="/blog" element={<Blog/>} />
-//         <Route path="/createpost" element={<CreatePost/>} />
+//           <Route
+//             path="/auth"
+//             element={<AuthComponent setIsLoggedIn={setIsLoggedIn} />}
+//           />
+//           <Route path="/blog" element={<Blog />} />
+//           <Route
+//             path="/createpost"
+//             element={ <ProtectedRoute
+//               path="/createpost"
+//               element={<CreatePost />}
+//               isLoggedIn={isLoggedIn}
+//             />}
+//           />
+//           {/* <Route path="/createpost" element={<CreatePost />} /> */}
+         
+//           <Route
+//             path="/category/health"
+//             element={<ProtectedRoute token={storedToken} element={Health} />}
+//           />
+//           <Route
+//             path="/category/travel"
+//             element={<ProtectedRoute token={storedToken} element={Travel} />}
+//           />
+//           <Route
+//             path="/category/music"
+//             element={<ProtectedRoute token={storedToken} element={Music} />}
+//           />
+//           <Route
+//             path="/category/art"
+//             element={<ProtectedRoute token={storedToken} element={Art} />}
+//           />
+//           <Route
+//             path="/category/education"
+//             element={<ProtectedRoute token={storedToken} element={Education} />}
+//           />
+//           <Route
+//             path="/category/fashion"
+//             element={<ProtectedRoute token={storedToken} element={Fashion} />}
+//           />
+//           <Route
+//             path="/category/other"
+//             element={<ProtectedRoute token={storedToken} element={Other} />}
+//           />
+//           <Route
+//             path="/category/science"
+//             element={<ProtectedRoute token={storedToken} element={Science} />}
+//           />
+//           <Route
+//             path="/category/sports"
+//             element={<ProtectedRoute token={storedToken} element={Sports} />}
+//           />
+//           <Route
+//             path="/category/technology"
+//             element={<ProtectedRoute token={storedToken} element={Technology} />}
+//           />
 //         </Routes>
+
+//         <Footer />
 //       </div>
-
-//       <Footer/>
-
 //     </Router>
 //   );
 // }
 
 // export default App;
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
@@ -42,18 +132,20 @@ import AuthComponent from "./components/register/AuthComponent";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Blog from "./components/Blogs/Blogs";
 import CreatePost from "./components/createpost/createpost";
-import Health from './components/Categories/Health';
-import Travel from './components/Categories/Travel';
-import Music from './components/Categories/Music';
-import Art from './components/Categories/Art';
-import Education from './components/Categories/Education';
-import Fashion from './components/Categories/Fashion'
-import Other from './components/Categories/Other'
-import Science from './components/Categories/Science'
-import Sports from './components/Categories/Sports'
-import Technology from './components/Categories/Technology'
+import Health from "./components/Categories/Health";
+import Travel from "./components/Categories/Travel";
+import Music from "./components/Categories/Music";
+import Art from "./components/Categories/Art";
+import Education from "./components/Categories/Education";
+import Fashion from "./components/Categories/Fashion";
+import Other from "./components/Categories/Other";
+import Science from "./components/Categories/Science";
+import Sports from "./components/Categories/Sports";
+import Technology from "./components/Categories/Technology";
 
 function App() {
+  const storedToken = localStorage.getItem("authToken"); // Retrieve Bearer token from local storage
+
   return (
     <Router>
       <div className="App">
--- client/src/components/Blogs/Blogs.js
+++ client/src/components/Blogs/Blogs.js
@@ -1,18 +1,58 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// const Blog = () => {
+//   const [posts, setPosts] = useState([]);
+
+//   useEffect(() => {
+//     axios.get('http://localhost:3001/posts') // Replace with your backend URL
+//       .then(response => {
+//         setPosts(response.data);
+//       })
+//       .catch(error => {
+//         console.error('Error fetching posts:', error);
+//       });
+//   }, []);
+
+//   return (
+//     <div className="blog">
+//       <h2>Latest Blog Posts</h2>
+//       <div className="post-list">
+//         {posts.map((post, index) => (
+//           <div key={index} className="post">
+//             <h3 className="post-title">{post.title}</h3>
+//             <p className="post-author">By {post.author}</p>
+//             <div className="post-content">{post.content}</div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Blog;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
+  const authToken = localStorage.getItem('authToken'); // Assuming you store the authentication token in local storage
 
   useEffect(() => {
-    axios.get('http://localhost:3000/posts') // Replace with your backend URL
-      .then(response => {
-        setPosts(response.data);
+    if (authToken) {
+      axios.get('http://localhost:3001/posts', {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
       })
-      .catch(error => {
-        console.error('Error fetching posts:', error);
-      });
-  }, []);
+        .then(response => {
+          setPosts(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching posts:', error);
+        });
+    }
+  }, [authToken]);
 
   return (
     <div className="blog">
